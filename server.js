e8836fdbf4f91c9dd4a13f13b633e2f73458b19c
Alexandr Rebikov <rebikovlk@mail.ru>
2022-01-31 17:34:29 +0700

    in-out json to url

--- server.js
+++ server.js
@@ -9,9 +9,12 @@ const { v4: uuidv4 } = require('uuid');
 const router = new Router();
 const app = new Koa();
 const store = require('./storage');
+
 const uploads = path.join(__dirname, '/uploads');
 
 app.use(koaStatic(uploads));
+
+let list = fs.readdirSync(uploads);
 
 app.use(koaBody({
     text: true,
@@ -35,56 +38,58 @@ router.get('/', async (ctx) => {
   //     ctx.response.body = JSON.stringify(list);
   //     return;
   // }
+  console.log(uploads);
   console.log(ctx.response);
   console.log(ctx.request);
     ctx.status = 200;
 });
 
+router.post('/download', async (ctx) => {
+  const name = ctx.request.body;
+  list = fs.readdirSync(uploads);
+  list.forEach((elem) => {
+    console.log(elem === name);
+    if (elem === name) {
+      ctx.response.body = JSON.stringify(elem);
+      // console.log(elem, list[i]);
+    }
+  });
+  ctx.status = 200;
+});
+
 router.post('/messages', async (ctx) => {
-  // console.log('response:', ctx.response);
-  console.log('request:', ctx.request.body);
-  store.push(ctx.request.body);
-  // console.log(store);
+  const message = JSON.parse(ctx.request.body);
+  store.push(message);
   ctx.status = 200;
 })
 
 router.post('/uploads', async (ctx) => {
-  console.log(ctx.request.files.file);
-
-  // const readStream = new fs.ReadStream(ctx.request)
   // const { file } = ctx.request.files;
+  const resp = JSON.parse(ctx.request.body);
+  store.push(resp);
+  console.log(store);
+  // const link = await new Promise((resolve, reject) => {
+  //   const oldPath = file.path;
+  //   const filename = file.name;
+  //   console.log(filename);
+  //   const newPath = path.join(uploads, filename);
 
-  // if (ctx.request.files.file) {
-  //     const filename = uuidv4();
-  //     const link = await new Promise((resolve) => {
-  //       const oldPath = file.path;
-  //       const filename = uuidv4();
-  //       const newPath = path.join(uploads, filename);
-  //       const readStream = fs.createReadStream(oldPath);
-  //       const writeStream = fs.createWriteStream(newPath);
-  //       readStream.on('close', () => {
-  //         fs.unlink(oldPath, (err) => {
-  //             if (err) {
-  //                 console.log(err);
-  //             }
-  //         });
-  //         resolve(filename);
-  //       });
-  //       readStream.pipe(writeStream);
-  //     });
-  // } else {
-  //     const url = ctx.request.body.url;
-  //     const filename = uuidv4();
-  //     https.get(url, (res) => {
-  //         const path = `${__dirname}/uploads/${filename}`; 
-  //         const filePath = fs.createWriteStream(path);
-  //         res.pipe(filePath);
-  //         filePath.on('finish',() => {
-  //             filePath.close();
-  //             console.log('Download Completed'); 
-  //         })
-  //     })
-  // }
+  //   const callback = (error) => reject(error);
+
+  //   const readStream = fs.createReadStream(oldPath);
+  //   const writeStream = fs.createWriteStream(newPath);
+
+  //   readStream.on('error', callback);
+  //   writeStream.on('error', callback);
+
+  //   readStream.on('close', () => {
+  //     console.log('close');
+  //     fs.unlink(oldPath, callback);
+  //     resolve(filename);
+  //   });
+
+  //   readStream.pipe(writeStream);
+  // });
 
   // list = fs.readdirSync(uploads);
   ctx.response.status = 200;
